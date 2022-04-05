21c9a6d9e394d0bee4b8513c89b8535f3d6dbb77
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 07:56:44 +0800

    Removed editor interface and launcher

--- editor/interface.go
+++
@@ -1,8 +0,0 @@
-package editor
-
-type Interface interface {
-	GetLogger() Logger
-	OpenProject(project any) // TODO: Mlog project.
-	CloseProject()
-	ListProjects(projects []any) // TODO: Mlog project manifests.
-}
