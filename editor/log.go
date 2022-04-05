77100e95cd9b904baf5d95f32a5506931a1de0cf
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 04:51:44 +0800

    Forgot to make add pointer for logrus

--- editor/log.go
+++ editor/log.go
@@ -2,7 +2,7 @@ package editor
 
 import "github.com/sirupsen/logrus"
 
-func InitLogrus(logger logrus.Logger) {
+func InitLogrus(logger *logrus.Logger) {
 	formatter := logrus.TextFormatter{
 		FullTimestamp: true,
 	}
