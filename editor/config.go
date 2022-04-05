61e677d7b331e60663959f4208c8f998b71e6d8e
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 02:51:11 +0800

    Added config loader

--- editor/config.go
+++ editor/config.go
@@ -1,5 +1,11 @@
 package editor
 
+import (
+	"encoding/json"
+	"flag"
+	"os"
+)
+
 type OnLaunch string
 
 const (
@@ -13,11 +19,31 @@ type Config struct {
 	OnLaunch     OnLaunch
 }
 
+const (
+	dataPath = "~/.mloggo/"
+)
+
 func DefaultConfig() Config {
 	return Config{
 		ProjectPaths: []string{
-			"~/.mloggo/projects",
+			dataPath + "projects",
 		},
 		OnLaunch: OpenLastProject,
 	}
+}
+
+// LoadConfig should only be used on desktop.
+func LoadConfig() (config Config, err error) {
+	const (
+		usage        = "Raw JSON data or a file path."
+		defaultValue = dataPath + "config.json"
+	)
+	argument := *flag.String("config", defaultValue, usage)
+	config = DefaultConfig()
+	var data []byte
+	if data, err = os.ReadFile(argument); err != nil {
+		data = []byte(argument)
+	}
+	err = json.Unmarshal(data, &config)
+	return
 }
