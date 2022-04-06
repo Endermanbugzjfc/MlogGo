c6ffb12aeb92f878745b2b95120b53ff9befc64d
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 12:42:25 +0800

    Ok I do not want to mess with compiler directives

--- mlog/codeblock.go
+++ mlog/codeblock.go
@@ -13,62 +13,18 @@ type CodeBlock interface {
 	ColourTview() int32
 }
 
-//lint:ignore U1000 Function is used through compiler directives.
-func Pink() int32 {
-	return 0xA08A8A
-}
+const (
+	pink   = 0xA08A8A
+	red    = 0xD4816B
+	purple = 0x877BAD
+	blue   = 0x6BB2B2
+	yellow = 0xC7B59D
+)
 
-//lint:ignore U1000 Function is used through compiler directives.
-func PinkTview() int32 {
-	return tcell.GetColor("pink").Hex()
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func Red() int32 {
-	return 0xA08A8A
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func RedTview() int32 {
-	return tcell.GetColor("pink").Hex()
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func Red() int32 {
-	return 0xD4816B
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func RedTview() int32 {
-	return tcell.GetColor("red").Hex()
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func Purple() int32 {
-	return 0x877BAD
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func PurpleTview() int32 {
-	return tcell.GetColor("purple").Hex()
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func Blue() int32 {
-	return 0x6BB2B2
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func BlueTview() int32 {
-	return tcell.GetColor("blue").Hex()
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func Yellow() int32 {
-	return 0xC7B59D
-}
-
-//lint:ignore U1000 Function is used through compiler directives.
-func YellowTview() int32 {
-	return tcell.GetColor("yellow").Hex()
-}
+var (
+	pinkTview   = tcell.GetColor("pink").Hex()
+	redTview    = tcell.GetColor("red").Hex()
+	purpleTview = tcell.GetColor("purple").Hex()
+	blueTview   = tcell.GetColor("blue").Hex()
+	yellowTview = tcell.GetColor("yellow").Hex()
+)
