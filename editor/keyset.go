3c86c129a62a74a9f15500a87fad2b4fa80ea150
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 12:04:00 +0800

    Complex keyset support for Tcell event key

--- editor/keyset.go
+++ editor/keyset.go
@@ -1,6 +1,11 @@
 package editor
 
-import "github.com/df-mc/atomic"
+import (
+	"strings"
+
+	"github.com/df-mc/atomic"
+	"github.com/gdamore/tcell/v2"
+)
 
 type Mode int
 
@@ -16,8 +21,20 @@ var (
 
 type KeySet string
 
-func (keyset KeySet) MatchWithRune(toMatch rune) bool {
-	return rune(keyset[0]) == toMatch
-}
+func (keyset KeySet) MatchTcellEventKey(event *tcell.EventKey) bool {
+	name := event.Name()
+	split := strings.Split(name, "Rune[")
+	name = split[1]
 
-// TODO: Complex keysets support.
+	split = strings.Split(name, "]")
+	name = split[0]
+
+	keysetString := string(keyset)
+	if len(split) > 1 {
+		return name == keysetString
+	}
+
+	name = strings.ToLower(name)
+	keysetString = strings.ToLower(keysetString)
+	return name == keysetString
+}
