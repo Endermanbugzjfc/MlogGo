819508addc63b3fd7e264b000f217bd745dd5982
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-06 08:04:15 +0800

    Added type editor mode

--- editor/keyset.go
+++ editor/keyset.go
@@ -1,5 +1,19 @@
 package editor
 
+import "github.com/df-mc/atomic"
+
+type Mode int
+
+const (
+	ModeSuper Mode = iota
+	ModeBlockSuper
+	ModeBlockEdit
+)
+
+var (
+	mode atomic.Value[Mode]
+)
+
 type KeySet string
 
 func (keyset KeySet) MatchWithRune(toMatch rune) bool {
